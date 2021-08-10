bc6e829c5652cb508f7cbd08681d48ba88e44bea
unknown <terencetan@hotmail.ca>
2021-08-10 11:34:54 -0400

    added router

---
+++ model/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const watchSchema = new Schema({
+  ticker: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+});
+
+const portSchema = new Schema({
+  ticker: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    default: 0,
+  },
+  ammount: {
+    type: Number,
+  },
+});
+
+const userSchema = new Schema({
+  watch: [watchSchema],
+  portfolio: [portSchema],
+  googleId: String,
+  name: String,
+});
+
+module.exports = mongoose.model("Stocks", userSchema);
