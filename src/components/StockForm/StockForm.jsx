339c4b9e41ae69a0105749aac32ccb8f56a3f5d9
unknown <terencetan@hotmail.ca>
2021-09-12 19:25:33 -0400

    added api data for search form

--- src/components/StockForm/StockForm.jsx
+++ src/components/StockForm/StockForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useInputState from "../../hooks/handleChange";
 import axios from "axios";
 const token = process.env.REACT_APP_FMP_ID;
@@ -6,6 +6,9 @@ const rootURL = `https://financialmodelingprep.com/api/v3/quote/`;
 
 export default function StockForm(props) {
   const [ticker, updateTicker, resetTicker] = useInputState("");
+  const [quote, setQuote] = useState();
+  const [rating, setRating] = useState();
+  const [priceData, setPriceData] = useState([]);
 
   //Sends a post request with axios to save the ticker to the user's watchlist
   const addWatch = async (event) => {
@@ -34,7 +37,43 @@ export default function StockForm(props) {
     }
   };
 
-  const getData = async (event) => {};
+  const getData = async (event) => {
+    event.preventDefault();
+
+    var todayDate = new Date().toISOString().slice(0, 10);
+    let daysAndMonths = todayDate.slice(4, 10);
+    let lastYearDate = (new Date().getFullYear() - 1)
+      .toString()
+      .split("")
+      .concat(daysAndMonths)
+      .join("");
+
+    let quote = `https://financialmodelingprep.com/api/v3/quote/${ticker.toUpperCase()}?apikey=${token}`;
+    let rating = `https://financialmodelingprep.com/api/v3/rating/${ticker.toUpperCase()}?apikey=${token}`;
+    let priceData = `https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?from=${lastYearDate}&to=${todayDate}&apikey=${token}`;
+    let urls = [quote, rating, priceData];
+    let newUrls = [];
+
+    //Spreading the array and using Axios for the concurrent get request
+    function urlSpread() {
+      for (let url of urls) {
+        newUrls.push(axios.get(url));
+      }
+    }
+    urlSpread();
+
+    try {
+      await axios.all([...newUrls]).then((responseArr) => {
+        console.log(responseArr);
+
+        setQuote(responseArr[0].data[0]);
+        setRating(responseArr[1].data[0]);
+        setPriceData(responseArr[2].data);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -48,10 +87,13 @@ export default function StockForm(props) {
           id='ticker'
           name='ticker'
         />
-        <button>Search!</button>
-        {/* <button onClick={addPort}>Add to Portfolio</button>
-        <button onClick={addWatch}>Add to Watchlist</button> */}
+        <button onClick={getData}>Search!</button>
       </form>
     </div>
   );
+}
+
+{
+  /* <button onClick={addPort}>Add to Portfolio</button>
+        <button onClick={addWatch}>Add to Watchlist</button> */
 }
